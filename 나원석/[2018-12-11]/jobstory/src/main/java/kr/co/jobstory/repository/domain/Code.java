c1ee9d4fc22f3123b323dcd9ea9101c752f6d3bf
나원석 <nsk7647@gmail.com>
2018-12-11 18:13:29 +0900

    [2018-12-11-나원석] 공고디테일 및 지원현황
    1. user 이력서 디테일.jsp(뒤로가기 버튼 추가)
    2. user 이력서 controller(공고번호 받기 추가)
    3. company 디테일 ( 별점 수정 )
    4. company 마이페이지 리스트에서 지원현황버튼 구현
    5. company.xml 수정
    6. domain 추가 (Apply)
    7. company controller, service 수정

---
+++ 나원석/[2018-12-11]/jobstory/src/main/java/kr/co/jobstory/repository/domain/Code.java
@@ -0,0 +1,15 @@
+package kr.co.jobstory.repository.domain;
+
+public class Code {
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
