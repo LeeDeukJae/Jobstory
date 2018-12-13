9feab5d05c12270726227c8a96302ff6e6aee212
jinwoongBang <dlopo123@gmail.com>
2018-12-13 19:56:16 +0900

    [2018-12-13 방진웅] 1. User Resume controller 2. User Resume Service 3. User Scrap Controller 4. User Scrap Service
    5. Domain
    -  Resume 관련 도메인
    - Scrap
    
    6. Mapper
    - ResumeMapper
    - ScrapMapper
    
    7, Xml
    - resume.xml
    - scrap.xml
    
    8. JSP
    - resume 관련 jsp
    - scrap.jsp

---
+++ 방진웅/20181213_1/jobstory/src/main/java/kr/co/jobstory/mypage/user/service/CommunityMyselfService.java
@@ -0,0 +1,19 @@
+package kr.co.jobstory.mypage.user.service;
+
+import java.util.List;
+
+import kr.co.jobstory.repository.domain.MyselfBoard;
+import kr.co.jobstory.repository.domain.MyselfUpload;
+import kr.co.jobstory.repository.domain.CommunityPage;
+
+public interface CommunityMyselfService {
+	List<MyselfBoard> list(CommunityPage page);
+	int listCount();
+	MyselfBoard detail(int no);
+	void insertBoard(MyselfBoard board);
+	int deleteBoard(int no);
+	void viewCnt(int no);
+	
+	void uploadBoard(MyselfUpload upload);
+	List<MyselfUpload> fileUpload(int no);
+}
